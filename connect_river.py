ee978bae8473bb11f66617da779c706db3492a4d
njuRS <46549560+njuRS@users.noreply.github.com>
2019-02-28 21:52:18 +0800

    connect river code

---
+++ connect_river.py
@@ -0,0 +1,135 @@
+#license = "GPL"
+#version = "0.1"
+#maintainer = "Lu Xin"
+#email = "xinlu.nju@gmail.com"
+#status = "Production"
+#description = "delete error and connect river"
+
+import arcpy,os,sys
+from arcpy import env
+from arcpy.sa import *
+
+env.overwriteOutput=True
+arcpy.CheckOutExtension("Spatial")
+env.extent="MAXOF"
+arcpy.env.workspace = "E:\gra_stu\worlddemtest\sentinel2_Norway\mndwi\mndwi_connect.gdb"  # make sure it is not too long
+
+
+DEM = "t3000_acc_arcticdem_10m1"  #the coordinate must be the same
+RS_error = "T20_T100_MNDWI_T32VNN_20180727T104021_bandpass_gabor_cpo20" 
+Raster_RS = "T20_T100_MNDWI_T32VNN_20180727T104021_bandpass_gabor_cpo20_1"
+cellsize = 10                                       
+delete_area = 300
+
+##delete error
+buffer_DEM_clip = "buffer50_"+ DEM
+arcpy.Buffer_analysis( DEM , buffer_DEM_clip , "50 Meters", "FULL", "ROUND", "ALL")
+RS = "clip_" + RS_error
+arcpy.Intersect_analysis([RS_error,buffer_DEM_clip],RS,"ALL","0","LINE")
+#(RS_error,buffer_DEM_clip,RS)
+print "finish delete error"
+                                                             
+##snap DEM to RS
+arcpy.Snap_edit(DEM , [[RS , "EDGE", "50 Meters"]])
+print "finish snap"
+
+##Get RS and DEM raster
+buffer_DEM = "snaped_buffer50_"+ DEM
+arcpy.Buffer_analysis( DEM , buffer_DEM , "50 Meters", "FULL", "ROUND", "ALL")
+arcpy.AddField_management(buffer_DEM, "value", "LONG", "","")
+arcpy.CalculateField_management(buffer_DEM,"value","2","PYTHON_9.3")
+
+ToRaster_buffer_DEM = "To_Raster_" + buffer_DEM
+arcpy.PolygonToRaster_conversion(buffer_DEM,"value",ToRaster_buffer_DEM,"CELL_CENTER","value",cellsize)
+print "finish RS and DEM raster"
+
+
+extract_Raster_RS  ="clip_" + Raster_RS
+extract_Raster_RS1 = ExtractByMask(Raster_RS, buffer_DEM)
+extract_Raster_RS1.save(extract_Raster_RS)
+arcpy.CalculateStatistics_management(extract_Raster_RS)
+arcpy.BuildPyramids_management(extract_Raster_RS)
+print "finish clip RS"
+
+
+##Get RS and DEM vector
+cal_Raster_RS_DEM = Raster(ToRaster_buffer_DEM) + Raster(extract_Raster_RS)
+con_cal_Raster_RS_DEM = Con(cal_Raster_RS_DEM>=3,3)
+
+
+RS_Polygon = "RasterToPolygon" + RS
+arcpy.RasterToPolygon_conversion(con_cal_Raster_RS_DEM,RS_Polygon,"SIMPLIFY")
+
+
+
+layer3 = "delete_area" + RS_Polygon
+arcpy.MakeFeatureLayer_management( RS_Polygon ,layer3 )
+arcpy.SelectLayerByAttribute_management(layer3,"NEW_SELECTION",'"Shape_Area"<3000')
+if int(arcpy.GetCount_management(layer3).getOutput(0)) > 0:
+    arcpy.DeleteFeatures_management(layer3)
+print "Get Raster to Polygon"
+
+
+##Get Snapped DEM-modeled drainage network which coincided with RS-mapped river network 
+DEM_coincidewith_RS = "coincide_" + DEM
+arcpy.Clip_analysis(DEM,RS_Polygon,DEM_coincidewith_RS)
+split_DEM_coincidewith_RS = "split_" + DEM_coincidewith_RS
+arcpy.SplitLine_management(DEM_coincidewith_RS,split_DEM_coincidewith_RS)
+unsplit_DEM_coincidewith_RS = "unsplit_" + split_DEM_coincidewith_RS
+arcpy.UnsplitLine_management(split_DEM_coincidewith_RS,unsplit_DEM_coincidewith_RS)
+
+##Get Snapped DEM-modeled drainage network which not coincided with RS-mapped river network
+DEM_connect_RS = "connect_" + DEM
+arcpy.Erase_analysis(DEM,RS_Polygon,DEM_connect_RS)
+split_DEM_connect_RS = "split_" + DEM_connect_RS
+arcpy.SplitLine_management(DEM_connect_RS,split_DEM_connect_RS)
+unsplit_DEM_connect_RS = "unsplit_" + split_DEM_connect_RS
+arcpy.UnsplitLine_management(split_DEM_connect_RS,unsplit_DEM_connect_RS)
+print "Get coincided and connect line"
+
+##Get dangle point of RS
+RS_Point = "RS_Point_"+ RS
+arcpy.FeatureVerticesToPoints_management(unsplit_DEM_coincidewith_RS,RS_Point,"BOTH_ENDS")
+print "Get RS_Point"
+
+
+arcpy.DeleteIdentical_management(unsplit_DEM_connect_RS , ["Shape"],"0.01 Meters")
+
+flag=0
+field = arcpy.da.TableToNumPyArray (unsplit_DEM_connect_RS, "Shape_Length", skip_nulls=True)
+sum = field["Shape_Length"].sum() 
+sum1=0
+
+while sum != sum1 :
+    flag=flag+1
+    #Get dangle point of DEM
+    ST_DEM =   "Both_Ends" +str(flag)+ "_" + unsplit_DEM_connect_RS
+    arcpy.FeatureVerticesToPoints_management(unsplit_DEM_connect_RS,ST_DEM,"BOTH_ENDS")
+    Start_DEM =   "Start" +str(flag)+ "_" + unsplit_DEM_connect_RS
+    arcpy.FeatureVerticesToPoints_management(unsplit_DEM_connect_RS,Start_DEM,"START")
+    End_DEM =   "End" +str(flag)+ "_" +unsplit_DEM_connect_RS
+    arcpy.FeatureVerticesToPoints_management(unsplit_DEM_connect_RS,End_DEM,"END")
+
+    layer1 = "interlayer" + str(flag) + "_" +unsplit_DEM_connect_RS
+    arcpy.MakeFeatureLayer_management( Start_DEM ,layer1 )
+    arcpy.SelectLayerByLocation_management(layer1,"INTERSECT",End_DEM)
+    Inter_DEM = "Inter" + str(flag)+ "_" +unsplit_DEM_connect_RS
+    if int(arcpy.GetCount_management(layer1).getOutput(0)) > 0:
+        arcpy.CopyFeatures_management(layer1, Inter_DEM)
+    print "Get DEM point"
+
+    #delete
+    erase1 ="erase_inter" + str(flag)  
+    erase2 ="erase_RS" +str(flag) 
+    arcpy.Erase_analysis(ST_DEM , Inter_DEM ,erase1 ,"0.01 Meters" )
+    arcpy.Erase_analysis(erase1 , RS_Point ,erase2 ,"0.01 Meters" )
+
+    layer2 = "deletelayer" + str(flag) +"_" + unsplit_DEM_connect_RS
+    arcpy.MakeFeatureLayer_management(unsplit_DEM_connect_RS ,layer2 )
+    arcpy.SelectLayerByLocation_management(layer2,"CONTAINS",erase2)
+    if int(arcpy.GetCount_management(layer2).getOutput(0)) > 0:
+        arcpy.DeleteFeatures_management(layer2)
+    sum1=sum
+    field = arcpy.da.TableToNumPyArray (unsplit_DEM_connect_RS, "Shape_Length", skip_nulls=True)
+    sum = field["Shape_Length"].sum()
+    print "Delete"
